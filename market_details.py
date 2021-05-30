ffb3dbb5b8d8de1e874795604ab12e0cc74225c4
Prajwol Chhetri <prajwalc391@gmail.com>
2021-05-30 16:24:29 +0545

    Added the top 5 gainers and losers function plus the stock scraper to the bot

---
+++ market_details.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://www.sharesansar.com/today-share-price')  # getting response from the website
+soup = BeautifulSoup(res.text, 'html.parser')  # changes the string from res.text to soup object using html parser
+stock_table = soup.tbody  # getting the table containing stock details
+
+# getting the contents from the table
+company = stock_table.select('a')
+start = stock_table.select('td:nth-child(4)')
+highest = stock_table.select('td:nth-child(5)')
+lowest = stock_table.select('td:nth-child(6)')
+end = stock_table.select('td:nth-child(7)')
+change = stock_table.select('td:nth-child(15)')
+
+# creating lists to store top 5 gainers and losers
+top_gainers = []
+top_losers = []
+
+# creating a function that sorts the stocks according to their change
+def sort_stocks_by_change(stock_list):
+    return sorted(stock_list, key=lambda k: k['change'], reverse=True)
+
+
+#  creating a function to append the stock details to a list
+def create_custom_ml(company, start, highest, lowest, end, change):
+    stocks = []
+    for idx, item in enumerate(company):
+        name = company[idx].getText()
+        close_price = end[idx].getText()
+        diff_price = float(change[idx].getText())
+        high_price = highest[idx].getText()
+        low_price = lowest[idx].getText()
+        opening_price = start[idx].getText()
+        stocks.append({'company': name, 'open': opening_price, 'previous close': close_price, 'high': high_price,
+                       'low': low_price,
+                       'change': diff_price})
+    return sort_stocks_by_change(stocks)
+
+
+stocks_list = create_custom_ml(company, start, highest, lowest, end, change)
+
+
+def summary():
+    top_gainers.append(stocks_list[:4])
+    top_losers.append(list(reversed(stocks_list[-5:])))
+    return top_gainers, top_losers
--- nepse_bot.py
+++ nepse_bot.py
@@ -2,36 +2,41 @@ import os
 import discord
 # from keep_alive import keep_alive
 from check_time import check_time
+from market_details import summary, top_gainers, top_losers
 
 client = discord.Client()
+
+
 # token = os.environ['discord_token']
 
 
 @client.event
 async def on_ready():
-	print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
 
 
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
+    if message.author == client.user:
+        return
 
-	if message.content.startswith('hello'):
-		await message.channel.send('Hello!')
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
 
-	if "status" in message.content:
-		if check_time():
-			await message.channel.send('Market is currently open')
-		else:
-			await message.channel.send('Market is currently closed')
+    if "status" in message.content:
+        if check_time():
+            await message.channel.send('Market is currently open')
+        else:
+            await message.channel.send('Market is currently closed')
+
+    if "summary" in message.content:
+        summary()
+        await message.channel.send(f'Top gainers are:\n{top_gainers}')
+        await message.channel.send(f'Top losers are:\n{top_losers}')
 
 
 client.run('')
 
-
 """ remove comment when deploying real time """
 # keep_alive()
 # client.run(token)
-
-
