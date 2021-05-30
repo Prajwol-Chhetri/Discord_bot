c77f1734019e1b8bbd9401f4252f6c9e192cd8bd
Prajwol Chhetri <prajwalc391@gmail.com>
2021-05-30 15:42:16 +0545

    Fixed the mistake in check_time module

--- check_time.py
+++ check_time.py
@@ -1,26 +1,26 @@
 import nepali_datetime
 from datetime import time
 
-now = nepali_datetime.datetime.now()
-
-# setting the working day and time for nepse
-start_time = time(11,00)
-end_time = time(15,00)
-working_day = [6, 0, 1, 2, 3]
-
-# getting the current time and day
-today = now.weekday()
-current_time = nepali_datetime.datetime.time(now)
-
 
 def check_time():
+	now = nepali_datetime.datetime.now()
+
+	# setting the working day and time for nepse
+	start_time = time(11, 00)
+	end_time = time(15, 00)
+	working_day = [6, 0, 1, 2, 3]
+
+	# getting the current time and day
+	today = now.weekday()
+	current_time = nepali_datetime.datetime.time(now)
+
 	# validating if it is working day
 	if today in working_day:
 		# validating if it is working time
-		if current_time >= start_time and current_time <= end_time:
+		if start_time <= current_time <= end_time:
 			return True
 		else:
 			return False		
 	else:
 		return False
-		
+
--- nepse_bot.py
+++ nepse_bot.py
@@ -1,10 +1,10 @@
 import os
 import discord
-from keep_alive import keep_alive
+# from keep_alive import keep_alive
 from check_time import check_time
 
 client = discord.Client()
-token = os.environ['token']
+# token = os.environ['discord_token']
 
 
 @client.event
@@ -20,11 +20,18 @@ async def on_message(message):
 	if message.content.startswith('hello'):
 		await message.channel.send('Hello!')
 
-		if "status" in message.content:
-			if check_time():
-				await message.channel.send('Market is currently open')
-			else:
-				await message.channel.send('Market is currently closed')
+	if "status" in message.content:
+		if check_time():
+			await message.channel.send('Market is currently open')
+		else:
+			await message.channel.send('Market is currently closed')
 
-keep_alive()
-client.run(token)
+
+client.run('')
+
+
+""" remove comment when deploying real time """
+# keep_alive()
+# client.run(token)
+
+
