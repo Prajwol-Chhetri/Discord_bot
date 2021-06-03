2ca03d4cbeba3430fab15e1bd06201265cda63eb
Prajwol Chhetri <prajwalc391@gmail.com>
2021-06-03 17:50:03 +0545

    Added some comments

--- ss_scraper.py
+++ ss_scraper.py
@@ -1,13 +1,17 @@
+# IMPORTING THE REQUIRED MODULES TO SCRAPE REQUIRED DATA
 import requests
 from bs4 import BeautifulSoup
 
 
 def get_script_detail(symbol):
+    # this function returns the details of a certain company.
     res = requests.get('https://www.sharesansar.com/today-share-price')  # getting response from the website
     soup = BeautifulSoup(res.text, 'html.parser')  # changes the string from res.text to soup object using html parser
     stock_table = soup.tbody  # getting the table containing stock details
     tag = stock_table.find('a', string=symbol)
     script_data = tag.find_parent('tr')
+
+    # Creating a dictionary to store detail of the company.
     company = {}
     name = (script_data.select('a')[0]).get_text()
     start = (script_data.select('td:nth-child(4)')[0]).get_text()
